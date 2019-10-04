707f85bf66d67a90a7f214ef2b1dcd29d4fbab4a
isaac <pavel.budantsev@yahoo.com>
2019-10-04 20:43:46 +0300

    added authentication service

---
+++ src/services/AuthService.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+interface RegistrationResponse {
+  key: string
+}
+
+interface LoginResponse {
+  key: string
+}
+
+interface LogoutResponse {
+  detail: string
+}
+
+export default class AuthService {
+  private domain: string;
+
+  constructor(domain: string) {
+    this.domain = domain;
+  }
+
+  public register(username: string, email: string, password: string) : Promise<RegistrationResponse> {
+    const credentials = {
+      username: username,
+      email: email,
+      password1: password,
+      password2: password
+    };
+    return axios.post(`${this.domain}/rest-auth/registration/`, credentials);
+  }
+
+  public login(username: string, email: string, password: string) : Promise<LoginResponse> {
+    const credentials = {
+      username: username,
+      email: email,
+      password: password
+    };
+    return axios.post(`${this.domain}/rest-auth/login/`, credentials);
+  }
+
+  public logout() : Promise<LogoutResponse> {
+    return axios.post(`${this.domain}/rest-auth/logout/`);
+  }
+
+}
