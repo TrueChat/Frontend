f9af4a79778bba503621da4196fea20b68f63710
isaac <pavel.budantsev@yahoo.com>
2019-10-08 21:35:06 +0300

    added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import AuthFormInput from "./components/auth/AuthFormInput";
 
 export default class App extends React.Component {
 
+  state = {
+    value: ""
+  };
+
   render() {
-    return <div>React App</div>
+    return (
+      <div style={{
+        width: "300px",
+        margin: "100px auto"
+      }}>
+        <AuthFormInput
+          label="Label"
+          value={this.state.value}
+          changeHandler={value => {
+            this.setState(state => ({
+              ...state,
+              value
+            }))
+          }}
+        />
+      </div>
+    )
   }
 }
---
+++ src/components/auth/AuthFormInput.tsx
@@ -0,0 +1,28 @@
+import React, {ReactElement} from "react";
+import "./AuthFormInput.scss";
+
+type AuthFormInputProps = {
+  label: string|ReactElement,
+  value: string,
+  changeHandler: (value: string) => void
+}
+
+export default class AuthFormInput extends React.Component<AuthFormInputProps> {
+
+  render() {
+    const { label, value, changeHandler } = this.props;
+    return (
+      <div className="Auth-form-input">
+        <label>
+          <div className="label">{label}</div>
+          <input
+            className="input"
+            type="text"
+            value={value}
+            onChange={event => changeHandler(event.currentTarget.value)}
+          />
+        </label>
+      </div>
+    )
+  }
+}
