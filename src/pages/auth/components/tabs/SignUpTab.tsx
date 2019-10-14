51b9c927672a85c4714bfa47ebe4e5ff1e898c3d
isaac <pavel.budantsev@yahoo.com>
2019-10-14 21:27:31 +0300

    added validation on Sign Up tab

--- src/pages/auth/components/tabs/SignUpTab.tsx
+++ src/pages/auth/components/tabs/SignUpTab.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AuthFormInput from "../form-inputs/AuthFormInput";
 import SubmitButton from "../common/SubmitButton";
 import AuthFormCheckbox from "../form-inputs/AuthFormCheckbox";
+import ErrorMessage from "../common/ErrorMessage";
 
 export type SignUpData = {
   email: string,
@@ -23,11 +24,17 @@ export default class SignUpTab extends React.Component<SignUpTabProps> {
       confirmPassword: "",
       login: "",
       remember: false
+    },
+    isValid: true,
+    violations: {
+      passwordsDontMatch: false,
+      invalidEmail: false,
+      invalidLogin: false
     }
   };
 
   render() {
-    const { formData } = this.state;
+    const { formData, violations } = this.state;
     return (
       <div>
         <div className="tab-section">
@@ -36,6 +43,7 @@ export default class SignUpTab extends React.Component<SignUpTabProps> {
             value={formData.login}
             changeHandler={value => this.setField("login", value)}
           />
+          {violations.invalidLogin ? <ErrorMessage message={"invalid login"} /> : null}
         </div>
         <div className="tab-section">
           <AuthFormInput
@@ -44,6 +52,7 @@ export default class SignUpTab extends React.Component<SignUpTabProps> {
             type="email"
             changeHandler={value => this.setField("email", value)}
           />
+          {violations.invalidEmail ? <ErrorMessage message={"invalid email"} /> : null}
         </div>
         <div className="tab-section">
           <AuthFormInput
@@ -60,6 +69,7 @@ export default class SignUpTab extends React.Component<SignUpTabProps> {
             type="password"
             changeHandler={value => this.setField("confirmPassword", value)}
           />
+          {violations.passwordsDontMatch ? <ErrorMessage message={"passwords don't match"} /> : null}
         </div>
         <div className="tab-section">
           <AuthFormCheckbox
@@ -68,18 +78,52 @@ export default class SignUpTab extends React.Component<SignUpTabProps> {
           />
         </div>
         <div className="tab-section text-right">
-          {/* TODO: add validation */}
-          <SubmitButton onClick={event => {
-            this.validate();
-            this.props.onSubmit(this.state.formData);
+          <SubmitButton onClick={_ => {
+            this.validate(formData => {
+              this.props.onSubmit(formData);
+            });
           }}/>
         </div>
       </div>
     )
   }
 
-  private validate() {
+  private validate(onSuccess: (formData: SignUpData) => void) {
+    this.setState((state: any) => {
+      const formData = state.formData;
+      state.violations.invalidEmail = !this.emailIsValid(formData.email);
+      state.violations.invalidLogin = !this.loginIsValid(formData.login);
+      state.violations.passwordsDontMatch = !this.passwordsMatch(formData.password, formData.confirmPassword);
+      return state;
+    }, () => {
+      if (this.isValid()) {
+        onSuccess(this.state.formData);
+      }
+    });
+  }
 
+  private loginIsValid(login: string) {
+    return login.length !== 0;
+  }
+
+  private emailIsValid(email: string) {
+    if (email.length < 3) {
+      return false;
+    }
+    return email.indexOf("@") !== -1;
+  }
+
+  private passwordsMatch(p1: string, p2: string) {
+    return p1 === p2;
+  }
+
+  private isValid() {
+    for (let violation of Object.values(this.state.violations)) {
+      if (violation) {
+        return false;
+      }
+    }
+    return true;
   }
 
   setField = (field: string, value: any) => {
